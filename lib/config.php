11cec06df009350d917c9725e26d038d84446616
drew <drew.e.westphal@gmail.com>
2014-05-05 00:54:07 -0400

    bringing up to date

--- lib/config.php
+++ lib/config.php
@@ -2,38 +2,43 @@
 /**
  * Enable theme features
  */
-add_theme_support('root-relative-urls');    // Enable relative URLs
-add_theme_support('bootstrap-top-navbar');  // Enable Bootstrap's top navbar
-add_theme_support('bootstrap-gallery');     // Enable Bootstrap's thumbnails component on [gallery]
-add_theme_support('nice-search');           // Enable /?s= to /search/ redirect
+//add_theme_support('root-relative-urls');    // Enable relative URLs
+add_theme_support('bootstrap-top-navbar');
+// Enable Bootstrap's top navbar
+add_theme_support('bootstrap-gallery');
+// Enable Bootstrap's thumbnails component on [gallery]
+add_theme_support('nice-search');
+// Enable /?s= to /search/ redirect
 //add_theme_support('jquery-cdn');            // Enable to load jQuery from the Google CDN
 
 /**
  * Configuration values
  */
-define('GOOGLE_ANALYTICS_ID', ''); // UA-XXXXX-Y (Note: Universal Analytics only, not Classic Analytics)
-define('POST_EXCERPT_LENGTH', 40); // Length in words for excerpt_length filter (http://codex.wordpress.org/Plugin_API/Filter_Reference/excerpt_length)
+define('GOOGLE_ANALYTICS_ID', '');
+// UA-XXXXX-Y (Note: Universal Analytics only, not Classic Analytics)
+define('POST_EXCERPT_LENGTH', 40);
+// Length in words for excerpt_length filter (http://codex.wordpress.org/Plugin_API/Filter_Reference/excerpt_length)
 
 /**
  * .main classes
  */
 function roots_main_class() {
-  if (roots_display_sidebar()) {
-    // Classes on pages with the sidebar
-    $class = 'col-sm-8';
-  } else {
-    // Classes on full width pages
-    $class = 'col-sm-12';
-  }
+    if(roots_display_sidebar()) {
+        // Classes on pages with the sidebar
+        $class = 'col-sm-8';
+    } else {
+        // Classes on full width pages
+        $class = 'col-sm-12';
+    }
 
-  return $class;
+    return $class;
 }
 
 /**
  * .sidebar classes
  */
 function roots_sidebar_class() {
-  return 'col-sm-4';
+    return 'col-sm-4';
 }
 
 /**
@@ -42,7 +47,7 @@ function roots_sidebar_class() {
  * See lib/sidebar.php for more details
  */
 function roots_display_sidebar() {
-  $sidebar_config = new Roots_Sidebar(
+    $sidebar_config = new Roots_Sidebar(
     /**
      * Conditional tag checks (http://codex.wordpress.org/Conditional_Tags)
      * Any of these conditional tags that return true won't show the sidebar
@@ -52,21 +57,24 @@ function roots_display_sidebar() {
      * array('function_name', array('arg1', 'arg2'))
      *
      * The second element must be an array even if there's only 1 argument.
-     */
-    array(
-      'is_404',
-      'is_front_page'
+     */ array(
+        'is_404',
+        'is_front_page',
+        array(
+            'is_singular',
+            array('cc_page')
+        ),
     ),
     /**
      * Page template checks (via is_page_template())
      * Any of these page templates that return true won't show the sidebar
      */
     array(
-      'template-custom.php'
-    )
-  );
+        'template-custom.php',
+        'canvas.php',
+    ));
 
-  return apply_filters('roots_display_sidebar', $sidebar_config->display);
+    return apply_filters('roots_display_sidebar', $sidebar_config -> display);
 }
 
 /**
@@ -76,4 +84,5 @@ function roots_display_sidebar() {
  * Example: If the content area is 640px wide, set $content_width = 620; so images and videos will not overflow.
  * Default: 1140px is the default Bootstrap container width.
  */
-if (!isset($content_width)) { $content_width = 1140; }
+if(!isset($content_width)) { $content_width = 1140;
+}
--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '93d5fd3eeb6d773ac81f1cb892bae0f1');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '172c58b2c3eb6ae82d3d52ef766bd27b');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
